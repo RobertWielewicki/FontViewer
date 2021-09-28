30905af8f596071a9d0ae0413a5dc49c31f271a9
RobertWielewicki <r.k.wielewicki@gmail.com>
2021-09-28 10:20:10 +0200

    $BROWSER launches after script completion

--- fonttesterFull.sh
+++ fonttesterFull.sh
@@ -6,7 +6,7 @@ FONTDIR=${1:-"."}
 NUM=1
 FONTS=$( find "$FONTDIR" -type f -name "*.ttf" -o -name "*.otf" -o -name "*.woff" )
 FONTSNUM=$( echo "$FONTS" | wc -l )
-
+[ "$FONTS" == '' ] && echo "No fonts found!" &&  exit
 printf "<!DOCTYPE html><head><style>"  > FONTS.html
 printf "body{max-width:1000px;background-color:#F0F7F4; margin-left:auto;margin-right:auto;padding:0 10px;}.fix{clear:left;} .fl{}"  >> FONTS.html
 printf "h6 {margin: 20px 0 0 0;padding: 5px 0 0 15px;background-color:#32292f;color:#99e1d9;}"  >> FONTS.html
@@ -31,3 +31,4 @@ done
 printf "</body></html>"  >> FONTS.html
 $BROWSER --version 2>/dev/null 2>&1
 [ $? -eq 0 ] && $BROWSER FONTS.html >/dev/null 2>&1 &
+
